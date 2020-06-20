6ced8e74c9696925e3777bb608e3b3e28118aff7
yerinie0526 <yerine@naver.com>
2020-06-20 21:21:24 +0900

    Auction 모델, 매퍼, dao 추가

---
+++ src/main/java/summerpetstore/model/AuctionJPModel.java
@@ -0,0 +1,41 @@
+package summerpetstore.model;
+
+public class AuctionJPModel {
+	
+	int auctionId; //물품번호
+	String userId; //예시: yerinie0526'
+	int offerPrice;
+	
+	public AuctionJPModel() {
+		
+	}
+	public AuctionJPModel(int auctionId, String userId, int offerPrice) {
+		super();
+		this.auctionId = auctionId;
+		this.userId = userId;
+		this.offerPrice = offerPrice;
+	}
+	public int getAuctionId() {
+		return auctionId;
+	}
+	public void setAuctionId(int auctionId) {
+		this.auctionId = auctionId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getOfferPrice() {
+		return offerPrice;
+	}
+	public void setOfferPrice(int offerPrice) {
+		this.offerPrice = offerPrice;
+	} 
+	
+	
+	
+
+}
+
