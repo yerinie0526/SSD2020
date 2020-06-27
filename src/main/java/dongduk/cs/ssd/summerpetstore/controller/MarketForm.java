f1dedb2eb854c516d3d0d58ca042d9b22553d6a7
tjtjtjtj0123 <tjtjtjtj0123@naver.com>
2020-06-27 20:06:02 +0900

    syj : mRegister

--- src/main/java/dongduk/cs/ssd/summerpetstore/controller/MarketForm.java
+++ src/main/java/dongduk/cs/ssd/summerpetstore/controller/MarketForm.java
@@ -13,14 +13,46 @@ public class MarketForm implements Serializable{
 	public MarketForm() {
 		
 	}
-	
-	public MarketForm(int categoryId, int itemKind, int pMethod, String description, String name) {
+
+	public MarketForm(String categoryId, String itemKind, String pMethod, String description, String name) {
 		super();
 		this.categoryId = categoryId;
 		this.itemKind = itemKind;
 		this.pMethod = pMethod;
 		this.description = description;
 		this.name = name;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getItemKind() {
+		return itemKind;
+	}
+
+	public void setItemKind(String itemKind) {
+		this.itemKind = itemKind;
+	}
+
+	public String getpMethod() {
+		return pMethod;
+	}
+
+	public void setpMethod(String pMethod) {
+		this.pMethod = pMethod;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public String getName() {
@@ -30,30 +62,7 @@ public class MarketForm implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 
-	public int getCategoryId() {
-		return categoryId;
-	}
-	public void setCategoryId(int categoryId) {
-		this.categoryId = categoryId;
-	}
-	public int getItemKind() {
-		return itemKind;
-	}
-	public void setItemKind(int itemKind) {
-		this.itemKind = itemKind;
-	}
-	public int getpMethod() {
-		return pMethod;
-	}
-	public void setpMethod(int pMethod) {
-		this.pMethod = pMethod;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
 		
 }
